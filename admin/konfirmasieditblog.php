23d4c557516554cc90e26a50ca7476002d43db61
Nicky Erlangga Setiawan <44841392+NickyErlanggaZ@users.noreply.github.com>
2021-03-24 07:18:55 +0700

    Menerapkan backend pada menu blog

---
+++ admin/konfirmasieditblog.php
@@ -0,0 +1,21 @@
+<?php 
+    session_start();
+    include('koneksi/koneksi.php');
+    $id_user = $_SESSION['id_user'];
+    $id_kategori_blog = $_POST['kategori_blog'];
+    $judul = $_POST['judul'];
+    $isi = $_POST['isi'];
+    if(empty($id_kategori_blog)){
+        header("Location:editblog.php?notif=editkosong&jenis=kategoriblog");
+    }else if(empty($judul)){
+        header("Location:editblog.php?notif=editkosong&jenis=judul");
+    }else if(empty($isi)){
+        header("Location:editblog.php?notif=editkosong&jenis=isi");
+    }else{
+        $sql = "UPDATE `blog` SET 
+        `id_kategori_blog`='$id_kategori_blog',`id_user`='$id_user',`judul`='$judul',`isi`='$isi',`tanggal`=DATE_FORMAT(CURRENT_TIMESTAMP(), '%Y-%c-%d'))";
+        mysqli_query($koneksi, $sql);
+        $id_blog = mysqli_insert_id($koneksi);
+        header("Location:blog.php?notif=editberhasil");
+    }
+?>
---
+++ admin/konfirmasitambahblog.php
@@ -0,0 +1,21 @@
+<?php 
+    session_start();
+    include('koneksi/koneksi.php');
+    $id_user = $_SESSION['id_user'];
+    $id_kategori_blog = $_POST['kategori_blog'];
+    $judul = $_POST['judul'];
+    $isi = $_POST['isi'];
+    if(empty($id_kategori_blog)){
+        header("Location:tambahblog.php?notif=tambahkosong&jenis=kategoriblog");
+    }else if(empty($judul)){
+        header("Location:tambahblog.php?notif=tambahkosong&jenis=judul");
+    }else if(empty($isi)){
+        header("Location:tambahblog.php?notif=tambahkosong&jenis=isi");
+    }else{
+        $sql = "INSERT INTO `blog` 
+        (`id_kategori_blog`,`id_user`,`judul`,`isi`,`tanggal`) VALUES ('$id_kategori_blog','$id_user','$judul','$isi',DATE_FORMAT(CURRENT_TIMESTAMP(), '%Y-%c-%d'))";
+        mysqli_query($koneksi, $sql);
+        $id_blog = mysqli_insert_id($koneksi);
+        header("Location:blog.php?notif=tambahberhasil");
+    }
+?>
